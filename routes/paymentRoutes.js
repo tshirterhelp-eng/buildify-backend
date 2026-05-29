5deaf2c018738d295a7d871be64393f1a6bd7768
tshirterhelp-eng <tshirterhelp@gmail.com>
2026-05-30 00:46:22 +0530

    Update paymentRoutes.js

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -9,11 +9,22 @@ const {
   createOrder,
 } = require("../controllers/cashfreeController");
 
+const {
+  cashfreeWebhook,
+} = require("../controllers/paymentController");
+
+// Engineer creates payment order
 router.post(
   "/create-order",
   authMiddleware,
   roleMiddleware("engineer"),
   createOrder
+);
+
+// Cashfree webhook
+router.post(
+  "/webhook",
+  cashfreeWebhook
 );
 
 module.exports = router;
