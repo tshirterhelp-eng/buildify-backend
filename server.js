1a66e01302efff460f05f550be012618a215a67e
tshirterhelp-eng <tshirterhelp@gmail.com>
2026-05-30 09:42:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,8 +27,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-app.post("/api/payments/webhook", async (req, res) => {
-  console.log(req.body);
 
-  res.status(200).send("OK");
-});
